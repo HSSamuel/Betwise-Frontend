ab5ee4634d135b3331dc5345048b4397ed4c62c0
Hunsa Semako S. <smkmayomisamuel@gmail.com>
2025-06-20 22:55:41 +0100

    Updated

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -4,7 +4,9 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white text-center p-4 mt-auto">
       <div className="container mx-auto">
-        <p>&copy; {new Date().getFullYear()} BetWise. All Rights Reserved.</p>
+        <p>
+          &copy; {new Date().getFullYear()} BetWise. Built by HSSamuel.
+        </p>
         <p className="text-sm text-gray-400 mt-1">
           Please bet responsibly. For help, visit responsible gambling sites.
         </p>
