890099b5af79aa0df9cd4ac498e1dc23ffffd35a
Hunsa Semako S. <smkmayomisamuel@gmail.com>
2025-06-21 14:06:19 +0100

    Updated

--- src/services/socketService.js
+++ src/services/socketService.js
@@ -1,12 +1,14 @@
 import { io } from "socket.io-client";
 
-const URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+const URL =
+  import.meta.env.VITE_API_BASE_URL ||
+  "https://betwise-backend-w91p.onrender.com/api/v1";
 
 // The socket instance is now a function that accepts the token
 export const createSocket = (token) => {
   return io(URL, {
     autoConnect: false,
-    // Add the auth object to send the token with the connection request
+    path: "/betwise-socket/", // This MUST match the path on the server
     auth: {
       token,
     },
